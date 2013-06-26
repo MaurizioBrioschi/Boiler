31f1d9ac3b6f297a3ad6e375d067804173910ac2
ridesoft <maurizio.brioschi@ridesoft.org>
2013-06-26 15:12:04 +0200

    test unit

---
+++ test/bootstrap.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Boiler CMS software
+ * @author Maurizio Brioschi (maurizio.brioschi@ridesoft.org) 
+ * @version 0.1 
+ 
+ * (c) Maurizio Brioschi (maurizio.brioschi@ridesoft.org) 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// put your code here
+/*** error reporting on ***/
+
+ error_reporting(E_ALL);
+
+ /*** define the site path ***/
+include '../includes/conf.php';
+/*** include the controller class ***/
+include '../application/' . 'controller_base.class.php';
+
+/*** include the registry class ***/
+include '../application/' . 'registry.class.php';
+
+/*** include the router class ***/
+include  '../application/' . 'router.class.php';
+
+/*** include the template class ***/
+include  '../application/' . 'template.class.php';
+ 
+ 
+function __autoload($class_name) {
+    $file = "../model/".$class_name . '.class.php';
+    
+    /*$templates = __SITE_PATH . '/model/templates/' . $filename;*/
+    
+    if (file_exists($file) == false)
+    {
+        return false;
+    }
+    include ($file);
+}
+?>
